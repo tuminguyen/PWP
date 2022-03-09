a92692df6c1034367fbb4950fec0129a82a8ffda
tuminguyen <huongnm.ptit@gmail.com>
2022-03-09 22:04:20 +0200

    update db.Model according to db diagram changes

--- run.py
+++ run.py
@@ -3,3 +3,15 @@
 from src.app import app
 
 app.run()
+
+# [DONE] Home + signup + sending email=> H => (100%)
+#
+# [DONE] After signup page =>  H => (100%)
+#
+# [DONE] Forgot pwd => H => (100%)
+#
+# Login => H => 70%
+#
+# Update profile => H => 0%
+#
+# Booking confirm page(changing time???/ default time/ sending email) =>  H
